427ba1716df1d774b44c6a6532b6b18d0fe96269
Abhilash Mund <abhilash.sisa@outlook.com>
2017-05-08 14:40:12 +0530

    Adding TestProgram.cs

---
+++ TestProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<=5;i++)
+            {
+                Console.WriteLine("jenkins");
+            }
+            Console.ReadKey();
+        }
+    }
+}
